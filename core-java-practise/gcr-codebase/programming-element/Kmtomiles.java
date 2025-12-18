55b12ac33a10df4b90f392245601eb2008246947
chhailbihari22cs <chhailbihari.gla_cs22@gla.ac.in>
2025-12-18 23:18:00 +0530

    Kmtomiles

---
+++ core-java-practise/gcr-codebase/programming-element/Kmtomiles.java
@@ -0,0 +1,8 @@
+public class KmToMiles {
+    public static void main(String[] args) {
+        double kmValue = 10.8;
+        final double KM_TO_MILES = 1.6;
+        double mileValue = kmValue * KM_TO_MILES;
+        System.out.println(kmValue + " kilometers equals " + mileValue + " miles.");
+    }
+}
