fee1e3ceb77f9b58d61d09f185330e9be038eb37
chhailbihari22cs <chhailbihari.gla_cs22@gla.ac.in>
2025-12-26 12:21:50 +0530

    [Chhailbihari] Refactor Files shifted in another branch core-java-practice

--- core-java-practise/gcr-codebase/Programming-Elements/Kmtomiles.java
+++
@@ -1,8 +0,0 @@
-public class Kmtomiles {
-    public static void main(String[] args) {
-        double kmValue = 10.8;
-        final double KM_TO_MILES = 1.6;
-        double mileValue = kmValue * KM_TO_MILES;
-        System.out.println(kmValue + " kilometers equals " + mileValue + " miles.");
-    }
-}
