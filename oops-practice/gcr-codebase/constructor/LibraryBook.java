c21fff135c39d7ab206c3e74371f4a2a514d156f
chhailbihari22cs <chhailbihari.gla_cs22@gla.ac.in>
2025-12-31 20:32:26 +0530

    [Chhailbihari] Add . constructor level 1

---
+++ oops-practice/gcr-codebase/constructor/LibraryBook.java
@@ -0,0 +1,23 @@
+class LibraryBook {
+
+    private String title;
+    private String author;
+    private double price;
+    private boolean available;
+
+    LibraryBook(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.available = true;
+    }
+
+    void borrowBook() {
+        if (available) {
+            available = false;
+            System.out.println("Book borrowed successfully.");
+        } else {
+            System.out.println("Book is not available.");
+        }
+    }
+}
