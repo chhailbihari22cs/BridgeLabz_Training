3461f3d8b93e9681a260f101dbeee3941afaa3b8
chhailbihari22cs <chhailbihari.gla_cs22@gla.ac.in>
2026-01-16 16:41:52 +0530

    [Chhailbihari] Refactor selection sort

--- dsa-practice/gcr-codebase/sorting/selectionSort.java
+++ dsa-practice/gcr-codebase/sorting/selectionSort.java
@@ -1,47 +1,48 @@
 import java.util.*;
-public class selectionSort
-{
+
+public class selectionSort {
 	public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter the length of an array");
-	int length=sc.nextInt();
-	int [] arr = new int[length];//here we declear an array
-	
-	
-	//here we initialize an array
-	System.out.println("please enter the exam scores")
-	for(int i=0;i<length;i++)
-	{
-	    arr[i]=sc.nextInt();
-	}
-	
-	
-		System.out.println("\n ___________Array before sorting___________");
+		Scanner sc = new Scanner(System.in);
+
+		// Input array length
+		System.out.println("Enter the length of an array:");
+		int length = sc.nextInt();
+
+		// Declare and initialize the array
+		int[] arr = new int[length];
+		System.out.println("Please enter the exam scores:");
+		for (int i = 0; i < length; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		// Display array before sorting
+		System.out.println("\n___________Array before sorting___________");
 		display(arr);
-	
-	//here we implement logic of selection sorting
-	for(int i =0 ; i<arr.length-1;i++)
-	{
-	    int min_idx=i;
-	    for(int j=i+1;j<arr.length;j++)
-	    {
-	        if(arr[min_idx]>arr[j])
-	        {
-	            min_idx=j;
-	        }
-	    }
-	    int swap=arr[min_idx];
-	    arr[min_idx]=arr[i];
-	    arr[i]=swap;
+
+		// Selection Sort Logic
+		for (int i = 0; i < arr.length - 1; i++) {
+			int min_idx = i;
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[min_idx] > arr[j]) {
+					min_idx = j;
+				}
+			}
+			// Swap the elements
+			int swap = arr[min_idx];
+			arr[min_idx] = arr[i];
+			arr[i] = swap;
+		}
+
+		// Display array after sorting
+		System.out.println("\n___________Array after sorting___________");
+		display(arr);
 	}
-	System.out.println("\n ____________Array after sorting________________");
-	display(arr);
-	}
-	public static void display(int[]arr)
-	{
-	    for(int i=0;i<arr.length;i++)
-	    {
-   	    System.out.print(arr[i]+"\t");
-	    }
+
+	// Method to display the array
+	public static void display(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + "\t");
+		}
+		System.out.println();
 	}
 }
