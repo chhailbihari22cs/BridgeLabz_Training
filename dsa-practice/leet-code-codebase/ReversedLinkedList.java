f485507cad6655e3d7b43bcd7a191b57dfba5440
chhailbihari22cs <chhailbihari.gla_cs22@gla.ac.in>
2026-01-20 16:39:27 +0530

    [Chhailbihari] Refactored

--- dsa-practice/leet-code-codebase/ReversedLinkedList.java
+++ dsa-practice/leet-code-codebase/ReversedLinkedList.java
@@ -1,26 +1,33 @@
+class ListNode {
+    int val;
+    ListNode next;
 
-  public class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
- 
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 class Solution {
     public ListNode reverseList(ListNode head) {
-        return reverse(head,null);       
+        return reverse(head, null);
     }
-    public ListNode reverse(ListNode curr,ListNode prev)
-    {
-        if(curr==null)
-        {
+
+    public ListNode reverse(ListNode curr, ListNode prev) {
+        if (curr == null) {
             return prev;
         }
-        ListNode ans=reverse(curr.next,curr);
-        curr.next=prev;
+        ListNode ans = reverse(curr.next, curr);
+        curr.next = prev;
         return ans;
-        
+
     }
 
 }
