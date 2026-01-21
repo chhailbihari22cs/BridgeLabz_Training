085b052c37288d1cc9216383670b2558e91e04dc
chhailbihari22cs <chhailbihari.gla_cs22@gla.ac.in>
2026-01-21 11:16:33 +0530

    [Chhailbihari] Add . java-generics

---
+++ dsa-practice/gcr-codebase/java-generics/Warehouse.java
@@ -0,0 +1,103 @@
+import java.util.*;
+
+// Abstract base class
+abstract class WarehouseItem {
+    private String name;
+
+    public WarehouseItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract String getCategory();
+}
+
+// Electronics class
+class Electronics extends WarehouseItem {
+    public Electronics(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getCategory() {
+        return "Electronics";
+    }
+}
+
+// Groceries class
+class Groceries extends WarehouseItem {
+    public Groceries(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getCategory() {
+        return "Groceries";
+    }
+}
+
+// Furniture class
+class Furniture extends WarehouseItem {
+    public Furniture(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getCategory() {
+        return "Furniture";
+    }
+}
+
+// Generic Storage class with bounded type
+class Storage<T extends WarehouseItem> {
+    private List<T> items = new ArrayList<>();
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+}
+
+// Utility class using Wildcards
+class WarehouseUtils {
+
+    // Wildcard method
+    public static void displayItems(List<? extends WarehouseItem> items) {
+        for (WarehouseItem item : items) {
+            System.out.println(item.getName() + " - " + item.getCategory());
+        }
+    }
+}
+
+// Main class
+public class Warehouse {
+    public static void main(String[] args) {
+
+        Storage<Electronics> electronicsStorage = new Storage<>();
+        electronicsStorage.addItem(new Electronics("Laptop"));
+        electronicsStorage.addItem(new Electronics("Smartphone"));
+
+        Storage<Groceries> groceriesStorage = new Storage<>();
+        groceriesStorage.addItem(new Groceries("Rice"));
+        groceriesStorage.addItem(new Groceries("Milk"));
+
+        Storage<Furniture> furnitureStorage = new Storage<>();
+        furnitureStorage.addItem(new Furniture("Chair"));
+        furnitureStorage.addItem(new Furniture("Table"));
+
+        System.out.println("Electronics");
+        WarehouseUtils.displayItems(electronicsStorage.getItems());
+
+        System.out.println("\nGroceries");
+        WarehouseUtils.displayItems(groceriesStorage.getItems());
+
+        System.out.println("\nFurniture");
+        WarehouseUtils.displayItems(furnitureStorage.getItems());
+    }
+}
